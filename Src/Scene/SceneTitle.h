bf06a167cc146eb0776742ed3e6f2a00ba9ce50e
Hiro1979ym54 <144640069+Hiro1979ym54@users.noreply.github.com>
2025-05-28 13:00:44 +0900

    title

--- Src/Scene/SceneTitle.h
+++ Src/Scene/SceneTitle.h
@@ -2,6 +2,8 @@
 #include "DxLib.h"
 #include "Scene.h"
 
+#define SCENE_TITLE_TEXT "Data/Title/TitleText.png"
+
 //シーン情報
 class SceneTitle : public SceneBase
 {
@@ -9,6 +11,7 @@ private:
 	bool endFlag;
 	int m_TitleHndle;			//タイトル画像の変数
 	int m_BackHndl;				//タイトルの背景画像の変数
+	int m_Item;					//カード描画
 	int m_Time;					//描画のタイミング
 	int m_Count;				//カウント
 
